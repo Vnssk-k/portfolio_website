4c66e84fe326de9c63328610ce2a524ae13de3d3
Olga Hrustaljova <olgahrustaljova@mail.ru>
2025-01-11 19:29:25 +0100

    navigation styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import { Link, NavLink } from 'react-router'
+import "./Navbar.css"
  
 function Navbar() {
  
     return (
-      <div>
+      <div className="navbar">
         <ul>
-            <NavLink to="/">
+            <NavLink to="/" className="nav-link" activeStyle={{borderBottom: "1px solid #000"}}>
                 <p>
                     About
                 </p>
             </NavLink>
-            <NavLink to="/myworks">
+            <NavLink to="/myworks" className="nav-link" activeStyle={{borderBottom: "1px solid #000"}}>
                 <p>
                     My Works
                 </p>
             </NavLink>
-            <NavLink to="/contact">
+            <NavLink to="/contact" className="nav-link" activeStyle={{borderBottom: "1px solid #000"}}>
                 <p>
                     Contact
                 </p>
