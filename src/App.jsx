b4df4f3f38c888634a7d47f2f80a2164f33e41d5
Olga Hrustaljova <olgahrustaljova@mail.ru>
2025-01-11 18:51:57 +0100

    Created navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
+import { Route, Routes } from 'react-router'
+import Navbar from './components/Navbar'
 import './App.css'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyWorks from './pages/MyWorks'
 
 function App() {
- 
+
   return (
     <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myworks" element={<MyWorks />} />
+      </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
+import { Link, NavLink } from 'react-router'
+ 
 function Navbar() {
  
     return (
-      <>
-      </>
+      <div>
+        <ul>
+            <NavLink to="/">
+                <p>
+                    About
+                </p>
+            </NavLink>
+            <NavLink to="/myworks">
+                <p>
+                    My Works
+                </p>
+            </NavLink>
+            <NavLink to="/contact">
+                <p>
+                    Contact
+                </p>
+            </NavLink>
+        </ul>
+      </div>
     )
   }
   
